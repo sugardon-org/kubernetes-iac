ff5cb07cbde0374dd87363882cee2890eff919f1
sugardon <sugar2don@gmail.com>
2021-05-09 18:09:37 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# kubernetes iac
+
+https://www.pulumi.com/docs/get-started/kubernetes/
+
+# Run
+
+## local
+
+```bash
+$ yarn
+$ ENVIRONMENT=local
+$ pulumi config set environment $ENVIRONMENT
+$ pulumi up
+```
+
+## sugardon01
+
+TODO
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import * as pulumi from "@pulumi/pulumi"
+import * as k8s from "@pulumi/kubernetes";
+
+let config = new pulumi.Config();
+let env = config.require("environment");
+pulumi.log.info("environment : " + env)
+
+const appLabels = { app: "nginx" };
+const deployment = new k8s.apps.v1.Deployment("nginx", {
+    spec: {
+        selector: { matchLabels: appLabels },
+        replicas: 1,
+        template: {
+            metadata: { labels: appLabels },
+            spec: { containers: [{ name: "nginx", image: "nginx" }] }
+        }
+    }
+});
+export const name = deployment.metadata.name;
