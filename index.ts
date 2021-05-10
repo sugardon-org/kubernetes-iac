61007d6327e493fd0219af1992c4eaefa5e05bde
sugardon <sugar2don@gmail.com>
2021-05-10 17:56:34 +0900

    add prettier

--- index.ts
+++ index.ts
@@ -1,13 +1,12 @@
-import * as pulumi from "@pulumi/pulumi"
-import * as k8s from "@pulumi/kubernetes";
+import * as pulumi from "@pulumi/pulumi";
 import { Metallb } from "./src/metallb";
 
-let config = new pulumi.Config();
-let env = config.require("environment");
-pulumi.log.info("environment : " + env)
+const config = new pulumi.Config();
+const env = config.require("environment");
+pulumi.log.info("environment : " + env);
 
 const metallb = new Metallb("metallb", {
-    environment: env,
-    kustomizePath: "./kustomize/metallb/overlays/" + env
-})
+  environment: env,
+  kustomizePath: "./kustomize/metallb/overlays/" + env,
+});
 export const kustomizeUrn = metallb.kustomizeUrn;
