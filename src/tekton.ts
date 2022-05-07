865ade85450576d07c2ef182b9485bc1cdebefbe
sugardon <sugar2don@gmail.com>
2022-05-08 01:51:08 +0900

    Update tekton operator

--- src/tekton.ts
+++ src/tekton.ts
@@ -6,6 +6,13 @@ interface tektonProps {
   kustomizePath: string;
 }
 
+interface DefaultProps {
+  namespace: string;
+}
+const dp: DefaultProps = {
+  namespace: "tekton-cicd",
+};
+
 export class Tekton extends pulumi.ComponentResource {
   public kustomizeUrn: pulumi.Output<string>;
 
@@ -16,11 +23,41 @@ export class Tekton extends pulumi.ComponentResource {
   ) {
     super("kubernetes:kustomize:tekton", name, {}, opts);
 
-    const dir = new k8s.kustomize.Directory(props.kustomizePath, {
-      directory: props.kustomizePath,
+    const operator = new k8s.yaml.ConfigFile("operator", {
+      file: "https://storage.googleapis.com/tekton-releases/operator/previous/v0.57.0/release.yaml",
     });
 
-    this.kustomizeUrn = dir.urn;
+    // https://github.com/tektoncd/operator/blob/main/docs/TektonConfig.md
+    const tektonConfig = new k8s.apiextensions.CustomResource(
+      "config",
+      {
+        apiVersion: "operator.tekton.dev/v1alpha1",
+        kind: "TektonConfig",
+        metadata: {
+          name: "config",
+          namespace: dp.namespace,
+        },
+        spec: {
+          profile: "all",
+          targetNamespace: dp.namespace,
+        },
+      },
+      {
+        dependsOn: [operator],
+        deleteBeforeReplace: true,
+      }
+    );
+
+    // TODO: Migrate kustomize to pulumi code
+    const config = new k8s.kustomize.Directory(
+      props.kustomizePath,
+      {
+        directory: props.kustomizePath,
+      },
+      { dependsOn: [operator, tektonConfig] }
+    );
+
+    this.kustomizeUrn = config.urn;
     this.registerOutputs();
   }
 }
